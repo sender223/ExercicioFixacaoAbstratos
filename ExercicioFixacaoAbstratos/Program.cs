3af8be6911532cbd9daf20a396e7bb5fb70a6126
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2019-10-09 20:04:31 -0300

    Add project files.

---
+++ ExercicioFixacaoAbstratos/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExercicioFixacaoAbstratos {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
